a5f56c760fbe4eb15c774c347cda9de1a627aa72
Calamia92 <119439868+Calamia92@users.noreply.github.com>
2025-04-11 10:38:51 +0200

    more db

--- db/docker-entrypoint-initdb.d/init.sql
+++ db/docker-entrypoint-initdb.d/init.sql
@@ -1,2 +1,14 @@
 -- Initialisation script de la base de données
 CREATE DATABASE mydb;
+
+CREATE TABLE IF NOT EXISTS items (
+                                     id SERIAL PRIMARY KEY,
+                                     title VARCHAR(255) NOT NULL,
+    description TEXT
+    );
+
+-- Insertion de quelques données pour tester
+INSERT INTO items (title, description) VALUES
+                                           ('Recette Spaghetti', 'Spaghetti avec sauce tomate et basilic'),
+                                           ('Citation Inspirante', 'La vie est un mystère qu’il faut vivre, et non un problème à résoudre'),
+                                           ('Anecdote Drôle', 'Une petite anecdote amusante pour égayer la journée');
